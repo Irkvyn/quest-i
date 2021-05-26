2a99a7c559932b88c3cc111adffa5c689d938678
Irkvyn <giannisleondiadis@gmail.com>
2021-05-27 02:30:53 +0300

    Serve React from build

--- server/index.js
+++ server/index.js
@@ -3,10 +3,13 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import logger from 'morgan';
 import path from 'path';
+import {fileURLToPath} from 'url';
 
 import usersRoutes from './routes/users.js'
 
 const port = process.env.PORT || 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 dotenv.config();
@@ -16,7 +19,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(logger('dev'));
 
 app.use('/users', usersRoutes);
-app.use('/', express.static('../client/build'));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(port, () => console.log(`Server running on port: ${port}`)))
