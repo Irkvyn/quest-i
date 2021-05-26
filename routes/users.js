8dd27194699174c7005c54efea52b315b2b4c83e
Irkvyn <giannisleondiadis@gmail.com>
2021-05-26 07:40:35 +0300

    Use dotenv

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const dotenv = require('dotenv');
+dotenv.config();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('This is ok!');
+    res.send(process.env.MONGODB_URI);
 });
 
 module.exports = router;
