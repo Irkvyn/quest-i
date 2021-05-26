cd47d763848eb29c50a59d18b3d836ed358ce447
Irkvyn <giannisleondiadis@gmail.com>
2021-05-26 05:07:37 +0300

    Connect with mongoDB Atlas

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const { MongoClient } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
+async function get() {
+    try {
+        await client.connect();
+        const cursor = client.db('quest-i').collection('users').find();
+        const results = await cursor.toArray();
+        console.log(results);
+        return results
+    } finally {
+        await client.close();
+    }
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    get().then(users => {
+        res.render('users', { users: users });
+    });
 });
 
 module.exports = router;
