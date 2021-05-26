0fa32219b276c9cb4289a9bb1a2cfaaf7587cfd8
Irkvyn <giannisleondiadis@gmail.com>
2021-05-26 06:47:48 +0300

    Remove mongoDB driver

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const { MongoClient } = require('mongodb');
-const uri = process.env.MONGODB_URI;
-
-async function get() {
-    const client = new MongoClient(uri,{ useUnifiedTopology: true });
-    try {
-        await client.connect();
-        const cursor = client.db('quest-i').collection('users').find();
-        const results = await cursor.toArray();
-        return results
-    } finally {
-        console.log('here');
-        await client.close();
-    }
-}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    get().then(users => {
-        res.render('users', { users: users });
-    });
+    res.send('This is ok!');
 });
 
 module.exports = router;
